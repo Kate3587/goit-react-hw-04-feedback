57da980df44a261bd245b738387ce2b379b8d5b2
Kate3587 <108617406+Kate3587@users.noreply.github.com>
2022-11-03 20:12:11 +0100

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Component } from 'react';
+import { useState } from 'react';
 import Section from '../Section'
 import FeedbackOptions from '../FeedbackOptions';
 import Notification from '../Notification'
@@ -8,16 +8,17 @@ import options from '../../data/Options';
 import { Container } from '../App/App.styled';
 
 
-class App extends Component{
-     state = {
-        good: 0,
-        neutral: 0,
-        bad: 0,
-        visible: false,
-     };
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [visible, setVisible] = useState(false);
 
-    handleChange = (event) => {
-      const { name } = event.target;
+          
+   const handleChange = (event) => {
+     const { name } = event.target;
+     
+     setVisible (true)
       
         this.setState(prevState => {
             return {
@@ -25,9 +26,8 @@ class App extends Component{
               visible: true,
             }})
     };
-        
-    render() {
-        const { good, neutral, bad} = this.state;
+      
+   
         const total = good + neutral + bad;
         const positivePercentage = (good &&  (good * 100 / total).toFixed(1));
 
@@ -36,17 +36,17 @@ class App extends Component{
               <Section title={"Please leave feedback"}>
                     <FeedbackOptions
                     options={options}
-                    onLeaveFeedback={this.handleChange}
+                    onLeaveFeedback={handleChange}
                     />
               </Section>
 
             <Section title={"Statistics"}>
-              {!this.state.visible && (
+              {!visible && (
                 <Notification
                 message={"There is no feedback"}
               />)}
               
-              {this.state.visible && (
+              {visible && (
                 <Statistics
                   good={good}
                   neutral={neutral}
@@ -60,7 +60,5 @@ class App extends Component{
             </Container>
         )
     };
-};
-
 
 export default App;
