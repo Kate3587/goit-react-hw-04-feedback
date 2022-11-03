e7c2ca39a40d9e38db939bc01d0648224a0786af
Kate3587 <108617406+Kate3587@users.noreply.github.com>
2022-11-03 22:42:33 +0100

    completed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { useState } from 'react';
+
+import { Container } from '../App/App.styled';
 import Section from '../Section'
 import FeedbackOptions from '../FeedbackOptions';
 import Notification from '../Notification'
 import Statistics from '../Statistics';
 import options from '../../data/Options';
-import { Container } from '../App/App.styled';
-
 
 function App() {
   const [good, setGood] = useState(0);
@@ -15,50 +15,56 @@ function App() {
   const [visible, setVisible] = useState(false);
 
           
-   const handleChange = (event) => {
-     const { name } = event.target;
-     
-     setVisible (true)
-      
-        this.setState(prevState => {
-            return {
-              [name]: prevState[name] + 1,
-              visible: true,
-            }})
-    };
-      
+  const handleChange = (event) => {
+    setVisible(true)
+    switch (event.target.name) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default: {
+        return;
+      }
+    }
+  };  
    
-        const total = good + neutral + bad;
-        const positivePercentage = (good &&  (good * 100 / total).toFixed(1));
+  const total = good + neutral + bad;
+  const positivePercentage = (good &&  (good * 100 / total).toFixed(1));
 
-        return (
-            <Container>
-              <Section title={"Please leave feedback"}>
-                    <FeedbackOptions
-                    options={options}
-                    onLeaveFeedback={handleChange}
-                    />
-              </Section>
+  return (
+    <Container>
+      <Section title={"Please leave feedback"}>
+        <FeedbackOptions
+          options={options}
+          onLeaveFeedback={handleChange}
+        />
+      </Section>
 
-            <Section title={"Statistics"}>
-              {!visible && (
-                <Notification
-                message={"There is no feedback"}
-              />)}
+      <Section title={"Statistics"}>
+        {!visible && (
+          <Notification
+            message={"There is no feedback"}
+          />)}
               
-              {visible && (
-                <Statistics
-                  good={good}
-                  neutral={neutral}
-                  bad={bad}
-                  total={total}
-                  positivePercentage={positivePercentage}
-                  />
-              )}
+        {visible && (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        )}
                  
-            </Section>
-            </Container>
-        )
-    };
+      </Section>
+    </Container>
+  );
+};
+
 
 export default App;
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,14 +5,14 @@ import { StatItem } from '../Statistics/Statistics.styled';
 
 const Statistics = ({good,neutral, bad, total, positivePercentage}) => (
 
-            <div>
-                <StatItem>Good: {good} </StatItem>
-                <StatItem>Neutral: {neutral}</StatItem>
-                <StatItem>Bad: {bad}</StatItem>
-                <StatItem>Total: {total}</StatItem>
-                <StatItem>Positive Feedback: {positivePercentage} %</StatItem>
-            </div>
-
+    <div>
+        <StatItem>Good: {good} </StatItem>
+        <StatItem>Neutral: {neutral}</StatItem>
+        <StatItem>Bad: {bad}</StatItem>
+        <StatItem>Total: {total}</StatItem>
+        <StatItem>Positive Feedback: {positivePercentage} %</StatItem>
+    </div>
+    
 );
 
 export default Statistics;
